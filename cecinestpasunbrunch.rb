43a4a434f6b3bc478aa78bde821c335e38cff838
Loïc CHOLLIER <loic@chollier.com>
2012-05-09 13:40:13 +0200

    lala5

--- cecinestpasunbrunch.rb
+++ cecinestpasunbrunch.rb
@@ -60,7 +60,7 @@ __END__
 @@ lyon69
 %h1 #Lyon69's recent photos
 @images.data.each do |media_item|
-  %img{:src => media_item.images.standard_resolution.url}
+  %strong= media_item.images.standard_resolution.url
 
 
 
