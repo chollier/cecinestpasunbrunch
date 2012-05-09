fa428ee59388d50c5281a19a4d0d382bb020d638
Loïc CHOLLIER <loic@chollier.com>
2012-05-09 13:32:06 +0200

    lala

--- cecinestpasunbrunch.rb
+++ cecinestpasunbrunch.rb
@@ -40,14 +40,14 @@ get "/lyon69" do
   
   @images = Instagram.tag_recent_media("lyon69")
   
-  # haml :lyon69
+  haml :lyon69, :format => :html5
  # images
  
- html = "<h1>#Lyon69's recent photos</h1>"
- for media_item in @images.data
-   html << "<img src='#{media_item.images.thumbnail.url}'>"
- end
- html
+ # html = "<h1>#Lyon69's recent photos</h1>"
+ # for media_item in @images.data
+ #   html << "<img src='#{media_item.images.thumbnail.url}'>"
+ # end
+ # html
 
 end
 
@@ -59,8 +59,8 @@ __END__
 
 @@ lyon69
 %h1 #Lyon69's recent photos
-for media_item in @images.data
-  # %img{:src => media_item.images.standard_resolution.url}
+@images.data.each do |media_item|
+  %img{:src => media_item.images.standard_resolution.url}
 end
 %div.title Hello world.
 
