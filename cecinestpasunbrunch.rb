1e36792585908586243f3ef89d4322182e6a92a3
Loïc CHOLLIER <loic@chollier.com>
2012-05-09 12:30:52 +0200

    trying lyon69 4

--- cecinestpasunbrunch.rb
+++ cecinestpasunbrunch.rb
@@ -37,12 +37,12 @@ end
 
 get "/lyon69" do
   
-  images = Instagram.tag_recent_media("lyon69")
+  @images = Instagram.tag_recent_media("lyon69")
   
  # images
  
  html = "<h1>#Lyon69's recent photos</h1>"
- for media_item in images
+ for media_item in Instagram.tag_recent_media("lyon69")
    html << "<img src='#{media_item.images.thumbnail.url}'>"
  end
  html
