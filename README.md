c5bac72233284a0ebe201bfcc31bea03d5b18837
Loïc CHOLLIER <loic@chollier.com>
2012-11-29 21:42:42 -0800

    Cleaning stuff for github & README

---
+++ README.md
@@ -0,0 +1,5 @@
+== Ceci n'est pas un Brunch (This is not a brunch) ==
+
+This is a tiny Sinatra website I made using Instagram API and a jQuery Plugin called Supersized to display on a screen every Instagram picture tagged with #Lyon69. 
+
+The main purpose of this website was to display picture taken by our reader at Lyon69.net on an event called "Ceci n'est pas un Brunch" ("This is not a brunch" which actually was).
--- cecinestpasunbrunch.rb
+++ cecinestpasunbrunch.rb
@@ -1,14 +1,13 @@
 require "sinatra"
 require "instagram"
-# require "haml"
 
 enable :sessions
 
 CALLBACK_URL = "http://cecinestpasunbrunch.heroku.com/oauth/callback"
 
 Instagram.configure do |config|
-  config.client_id = "a731e18132984f5a830980bc3b21f9c7"
-  config.client_secret = "cd9af3e8abcd4b828c6393b739efdca9"
+  config.client_id = "[REPLACE BY YOUR CLIENT ID]"
+  config.client_secret = "[REPLACE BY YOUR CLIENT SECRET]"
 end
 
 get "/" do
@@ -39,19 +38,9 @@ end
 get "/lyon69" do
   
   @images = Instagram.tag_recent_media("lyon69")
-  
-  # puts @images
-  
+    
   erb :demo
-  # haml :lyon69, :format => :html5
- # images
- 
- # html = "<h1>#Lyon69's recent photos</h1>"
- # for media_item in @images.data
- #   html << "<img src='#{media_item.images.thumbnail.url}'>"
- # end
- # html
-
+  
 end
 
 # __END__
